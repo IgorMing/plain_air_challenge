6b2e4781feb1b0b7f98272181bdf9e29d2d7ade4
Igor Ming <igor.ming@gmail.com>
2024-02-01 15:19:05 -0300

    exchange base from bare RN to expo

--- App.tsx
+++ App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, useColorScheme } from "react-native";
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import HomeScreen from './src/screens/Home';
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import HomeScreen from "./src/screens/Home";
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     flex: 1,
   };
-
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        style="dark"
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <HomeScreen />
     </SafeAreaView>
   );
 }
-
-export default App;
